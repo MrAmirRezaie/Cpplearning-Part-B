70ad485f3cb9ad894a32cc63e6c014b00abf9c5a
MrAmirRezaie <mramirrezaie80@gmail.com>
2024-03-13 01:02:52 -0700

    Add project files.

---
+++ Cpplearning-Part[B]/Cpplearning-Part[B].cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+//Part[B]
+
+//Write a program that prints the numbers between zero and hundred
+
+int main()
+{
+	for (int i = 0; i <= 100; i++)
+	{
+		cout << i << " ";
+	}
+
+	return 0;
+}
